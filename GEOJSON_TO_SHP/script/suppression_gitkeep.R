e81398e34b7a4e52a759a559e580a7950ebdb2e8
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2024-02-06 08:15:06 +0100

    Add files via upload

--- GEOJSON_TO_SHP/script/suppression_gitkeep.R
+++ GEOJSON_TO_SHP/script/suppression_gitkeep.R
@@ -1,7 +1,7 @@
 #################### Suppression des fichiers .gitkeep
 
-if(file.exists("data/.gitkeep"))
-  {file.remove("data/.gitkeep")}
+if(file.exists("data/gitkeep"))
+  {file.remove("data/gitkeep")}
 
-if(file.exists("result/.gitkeep"))
-  {file.remove("result/.gitkeep")}
+if(file.exists("result/gitkeep"))
+  {file.remove("result/gitkeep")}
